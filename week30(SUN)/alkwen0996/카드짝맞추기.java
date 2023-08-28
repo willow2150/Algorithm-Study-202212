2343c67c4e840588de4d072d7269d1d1fbeab5c9
minsulee <alkwen0996@naver.com>
2023-08-28 11:28:47 +0900

    Programmers_17679 alkwen0996 - Add 1nd Solution

---
+++ week30(SUN)/alkwen0996/카드짝맞추기.java
@@ -0,0 +1,15 @@
+public class 카드짝맞추기 {
+    public static void main(String[] args) {
+        int[][] board = {{1, 0, 0, 3}, {2, 0, 0, 0}, {0, 0, 0, 2}, {3, 0, 1, 0}};
+        int r = 1;
+        int c = 0;
+
+        System.out.println(solution(board, r, c));
+    }
+
+    private static int solution(final int[][] board, final int r, final int c) {
+
+        return 0;
+    }
+
+}
